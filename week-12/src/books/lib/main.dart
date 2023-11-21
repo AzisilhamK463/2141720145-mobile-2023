f534075e652117bd7205153c98248212e5f91b1f
AzisilhamK463 <azisirawan569@gmail.com>
2023-11-21 14:21:29 +0700

    initiation week 12 & 13

---
+++ week-12/src/books/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
